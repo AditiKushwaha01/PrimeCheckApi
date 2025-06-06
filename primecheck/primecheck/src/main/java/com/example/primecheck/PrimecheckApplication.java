6b5359f4a17529a177df861eff7d0b4cc1102100
Aditi Kushwaha <137718055+AditiKushwaha01@users.noreply.github.com>
2025-06-07 00:23:16 +0530

    Add files via upload

---
+++ primecheck/primecheck/src/main/java/com/example/primecheck/PrimecheckApplication.java
@@ -0,0 +1,13 @@
+package com.example.primecheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimecheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimecheckApplication.class, args);
+	}
+
+}
